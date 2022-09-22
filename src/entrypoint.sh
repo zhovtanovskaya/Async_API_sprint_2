21cca50d276f93db74a11779f1bdf402030c8a12
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2022-09-22 09:16:24 +0500

    В entrypoint.sh установлен порт 8000 вместо 8500 #4

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,4 +3,4 @@
 # Запустить uvicorn  из директории, в которой находится этот скрипт.
 SCRIPT_DIR=$(dirname `realpath $0`)   # Путь к этому скрипту.
 cd $SCRIPT_DIR
-uvicorn main:app --host 0.0.0.0 --port 8500
+uvicorn main:app --host 0.0.0.0 --port 8000
