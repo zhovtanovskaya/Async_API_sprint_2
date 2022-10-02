39cf91db690c550cf5e5443cea006bc5eebdff36
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2022-10-02 09:51:57 +0500

    Добавлена фикстура es_client для test_search.es_write_data #14

--- tests/functional/src/test_search.py
+++ tests/functional/src/test_search.py
@@ -27,19 +27,21 @@ def get_es_bulk_query(data, elastic_index, elastic_id_field):
 
 
 @pytest.fixture
-def es_write_data():
+async def es_client():
+    client = AsyncElasticsearch(hosts='127.0.0.1:9200')
+    yield client
+    await client.close()
+
+
+@pytest.fixture
+def es_write_data(es_client):
     async def inner(data: list[dict]):
         bulk_query = get_es_bulk_query(
             data,
             test_settings.elastic_index,
             test_settings.elastic_id_field)
         str_query = '\n'.join(bulk_query) + '\n'
-
-        es_client = AsyncElasticsearch(hosts=test_settings.elastic_host,
-                                       validate_cert=False,
-                                       use_ssl=False)
         response = await es_client.bulk(str_query, refresh=True)
-        await es_client.close()
         if response['errors']:
             raise Exception('Ошибка записи данных в Elasticsearch')
     return inner
