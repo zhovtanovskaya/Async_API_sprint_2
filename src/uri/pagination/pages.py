2b8491068c98d57039f225ebfb7c0d57eb8831be
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2022-10-10 15:35:47 +0500

    Добавлены поля .limit и .offset в uri.pagination.pages.Page #25

--- src/uri/pagination/pages.py
+++ src/uri/pagination/pages.py
@@ -1,6 +1,6 @@
 """Информации о страницах данных REST API."""
 
-from pydantic import BaseModel, PositiveInt
+from pydantic import BaseModel, NonNegativeInt, PositiveInt
 
 
 class Page(BaseModel):
@@ -8,3 +8,24 @@ class Page(BaseModel):
 
     number: PositiveInt
     size: PositiveInt
+
+    @property
+    def limit(self) -> PositiveInt:
+        """Максимум элементов на странице.
+
+        Returns:
+            self.size.
+        """
+        return self.size
+
+    @property
+    def offset(self) -> NonNegativeInt:
+        """Индекс первого элемента этой страницы.
+
+        Подразумевается индекс в разбиваемом на странице списке элементов.
+
+        Returns:
+            Номер индекса.
+        """
+        return self.size * (self.number - 1)
+
