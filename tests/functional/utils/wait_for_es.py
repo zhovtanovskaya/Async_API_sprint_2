2492ff78487284c9bcdae2784a9af6cd7d73da34
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2022-10-10 20:57:01 +0500

    wait_for-скрипты для Docker Compose переписаны с @backoff #27

--- tests/functional/utils/wait_for_es.py
+++ tests/functional/utils/wait_for_es.py
@@ -1,15 +1,18 @@
 """Модуль для Docker, который дожидается старта Elastic."""
 
-import time
-
 from elasticsearch import Elasticsearch
 
 from tests.functional.settings import test_settings
+from tests.functional.utils.backoff import backoff
+
+
+@backoff(exceptions=(ConnectionError,))
+def ping_elastic(hosts: list[str]):
+    client = Elasticsearch(hosts=hosts, validate_cert=False, use_ssl=False)
+    if not client.ping():
+        raise ConnectionError()
+
 
 if __name__ == '__main__':
     hosts = [f'{test_settings.elastic_host}:{test_settings.elastic_port}']
-    es_client = Elasticsearch(hosts=hosts, validate_cert=False, use_ssl=False)
-    while True:
-        if es_client.ping():
-            break
-        time.sleep(1)
+    ping_elastic(hosts)
